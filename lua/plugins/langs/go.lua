d120de7dc43abf28fe20c0754a7690137056e2dd
Leonardo Gago <ljgago@gmail.com>
2025-01-15 20:26:25 -0300

    feat: disable lsp_codelens in plugins/langs/go.lua and update default tags

--- lua/plugins/langs/go.lua
+++ lua/plugins/langs/go.lua
@@ -5,14 +5,15 @@ return {
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  lazy = false,
+  -- lazy = false,
   -- priority = 1000,
   event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
   config = function()
     require("go").setup({
-      tag_transform = "camelcase",
-      tag_options = "json=",
+      tag_transform = "snakecase",
+      tag_options = "json=omitempty",
+      lsp_codelens = false,
     })
   end,
   -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
